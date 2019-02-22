485120e201982dba1675a5d91191567deedb83b5
Trent <trent.polenchar.2021@student.lhschools.org>
2019-02-22 13:56:13 -0500

    Added 8 Ball answers

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -12,34 +12,34 @@ public class Magic8Ball {
 
         testing = keyboard.nextInt();
         if((testing>=0)&(testing<=10)){
-            System.out.println("0-10");
+            System.out.println("It is certain.");
         }
         else if((testing>=11)&(testing<=20)){
-            System.out.println("11-20");
+            System.out.println("Signs point to yes.");
         }
         else if((testing>=21)&(testing<=30)){
-            System.out.println("21-30");
+            System.out.println("Don't count on it.");
         }
         else if((testing>31)&(testing<=40)){
-            System.out.println("31-40");
+            System.out.println("Without a doubt.");
         }
         else if((testing>=41)&(testing<=50)){
-            System.out.println("41-50");
+            System.out.println("Very doubtful.");
         }
         else if((testing>=51)&(testing<=60)){
-            System.out.println("51-60");
+            System.out.println("Outlook not so good.");
         }
         else if((testing>=61)&(testing<=70)){
-            System.out.println("61-70");
+            System.out.println("Definitely.");
         }
         else if((testing>=71)&(testing<=80)){
-            System.out.println("71-80");
+            System.out.println("My reply is no.");
         }
         else if((testing>=81)&(testing<=90)){
-            System.out.println("81-90");
+            System.out.println("Outlook good.");
         }
         else if((testing>=91)&(testing<=100)){
-            System.out.println("91-100");
+            System.out.println("Nope.");
         }
 
 
