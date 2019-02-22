228e19e737d56c55aa6450b6d0b13e9e129286fe
Trent <trent.polenchar.2021@student.lhschools.org>
2019-02-22 13:45:19 -0500

    Created else statements

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -5,7 +5,42 @@ public class Magic8Ball {
     public static void main(String[]args){
 
         Scanner keyboard;
-        
+        int testing;
+        String question;
+
+        keyboard = new Scanner(System.in);
+
+        testing = keyboard.nextInt();
+        if (testing>0){
+            System.out.println("0-10");
+        }
+        else if(testing>0){
+            System.out.println("11-20");
+        }
+        else if(testing>0){
+            System.out.println("21-30");
+        }
+        else if(testing>0){
+            System.out.println("31-40");
+        }
+        else if(testing>0){
+            System.out.println("41-50");
+        }
+        else if(testing>0){
+            System.out.println("51-60");
+        }
+        else if(testing>0){
+            System.out.println("61-70");
+        }
+        else if(testing>0){
+            System.out.println("71-80");
+        }
+        else if(testing>0){
+            System.out.println("81-90");
+        }
+        else if (testing>0){
+            System.out.println("91-100");
+        }
 
 
     }
